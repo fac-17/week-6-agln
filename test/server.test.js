6ad2f40794c918d004438c480807047d7eacc0db
sarah <sarahkharraz@gmail.com>
2019-08-07 15:52:38 +0100

    TEST 1:Home route should return a status code of 200

--- test/server.test.js
+++ test/server.test.js
@@ -1,1 +1,15 @@
-const test = require("supertest");
+const supertest = require("supertest");
+const tape = require("tape");
+const router = require("../src/router");
+
+tape('Home route should return a status code of 200', (t) => {
+  supertest(router)
+  .get('/')
+  .expect(200)
+  .expect('content-type', /html/)
+  .end((err, res) => {
+    t.error(err)
+    t.equal(res.statusCode, 200, 'Home route should return status code 200')
+    t.end()
+  })
+})
