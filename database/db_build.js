f8a14ad491127feff481fb0a28b112573713c066
Gregor <gregorlthomson@sky.com>
2019-08-08 12:43:36 +0100

    Correct Slasher Mistake

--- database/db_build.js
+++ database/db_build.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const dbConnection = require("./db_connection");
 
-const sql = fs.readFileSync(`${__dirname}\db_build.sql`).toString();
+const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
 dbConnection.query(sql, (err, res) => {
   if (err) throw err;
