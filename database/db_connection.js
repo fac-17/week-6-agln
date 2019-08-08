e34ed4d1ecacbcb2b354762fe85e8d06db25952b
gminova <minova1995@gmail.com>
2019-08-08 12:50:42 +0100

    fixed typo

--- database/db_connection.js
+++ database/db_connection.js
@@ -16,7 +16,7 @@ const [username, password] = params.auth.split(":");
 const options = {
   host: params.hostname,
   port: params.port,
-  database: params.pathname.split("/"[1]),
+  database: params.pathname.split("/")[1],
   max: process.env.DB_MAX_CONNECTIONS || 2
 };
 
