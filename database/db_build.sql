41d89d4192ad6b6542d9d8ec26070ea8785acf22
gminova <minova1995@gmail.com>
2019-08-08 12:37:32 +0100

    table of warriors

---
+++ database/db_build.sql
@@ -0,0 +1,17 @@
+BEGIN;
+
+DROP TABLE IF EXIST warriors (
+
+warrior_id SERIAL PRIMARY KEY,
+warrior_name VARCHAR (20) NOT NULL,
+warrior_c1 INTEGER,
+warrior_c2 INTEGER,
+warrior_c3 INTEGER
+);
+
+INSERT INTO warriors (warrior_name, warrior_c1, warrior_c2, warrior_c3) VALUES('catMaster24', 1, 0, 1);
+INSERT INTO warriors (warrior_name, warrior_c1, warrior_c2, warrior_c3) VALUES('cruiseVoyager25', 0, 0, 0);
+INSERT INTO warriors (warrior_name, warrior_c1, warrior_c2, warrior_c3) VALUES('beardedWizard', 1, 0, 0);
+INSERT INTO warriors (warrior_name, warrior_c1, warrior_c2, warrior_c3) VALUES('glansFromFrance', 1, 1, 1);
+
+COMMIT;
