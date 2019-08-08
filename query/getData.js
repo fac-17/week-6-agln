e4f24cd217363ac0f2c7f8f971da79179d35eab7
georgia.shaw <georgia@cranbrookw4.com>
2019-08-08 15:04:47 +0100

    creating sum of warrior_c1, warrior_2 and warrior_c3 columns' git push origin Feature/sum

--- query/getData.js
+++ query/getData.js
@@ -1,7 +1,7 @@
 const databaseConnection = require('../database/db_connection.js');
 
 const getSum = cb => {
-  databaseConnection.query('SELECT warrior_c1, warrior_c2, warrior_c3, (warrior_c1 + warrior_c2 + warrior_c3) AS "warrior_total" FROM warriors',
+  databaseConnection.query('SELECT warrior_name, (warrior_c1 + warrior_c2 + warrior_c3 + 1) AS "warrior_total" FROM warriors',
   (err, res) => {
     if (err) {
       cb(err);
