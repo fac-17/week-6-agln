b3dde75f5743bc2de68a8dc44c650e1575b9f4df
sarah <sarahkharraz@gmail.com>
2019-08-08 15:50:23 +0100

    Create the query returning the warrior name and the glan name

--- query/getData.js
+++ query/getData.js
@@ -1,7 +1,7 @@
 const databaseConnection = require('../database/db_connection.js');
 
 const getSum = cb => {
-  databaseConnection.query('SELECT warrior_name, (warrior_c1 + warrior_c2 + warrior_c3 + 1) AS "warrior_total" FROM warriors',
+  databaseConnection.query(`SELECT glans.glan_name, total.warrior_name FROM glans INNER JOIN (SELECT warrior_name, (warrior_c1 + warrior_c2 + warrior_c3 + 1) AS warrior_total FROM warriors) AS total ON glans.glan_id = total.warrior_total;`),
   (err, res) => {
     if (err) {
       cb(err);
