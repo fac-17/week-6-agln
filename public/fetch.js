dd23240dc7c065d8b8fc7dbb896be92cc72e9efb
Gregor <gregorlthomson@sky.com>
2019-08-08 12:27:30 +0100

    Add XML Request

--- public/fetch.js
+++ public/fetch.js
@@ -1,35 +1,37 @@
-const domRequest = (url) => {
-  // let userInput = encodeURIComponent(input);
-  //Clean the input and prevent script injections
-  fetch(url)
-  .then(res => res.json())
-  .then(json => resToFrontEnd(input, json))
-}
+let responseOutput = [];
 
-let submit = document.querySelector('#submit__button');
-submit.addEventListener('click', (e) => {
-  let name = document.querySelector('#membername').value;
-  let listOfCategories = document.querySelectorAll('.radioButton');
+let submit = document.querySelector("#submit__button");
+submit.addEventListener("click", e => {
+  let name = document.querySelector("#membername").value;
+  let listOfCategories = document.querySelectorAll(".radioButton");
   console.log(listOfCategories);
-let selected = [];
-listOfCategories.forEach(element => {
-if (element.checked === true) {
-  selected.push(element.value);
-};
-});
-
+  let selected = [];
+  listOfCategories.forEach(element => {
+    if (element.checked === true) {
+      selected.push(element.value);
+    }
+  });
 
   const url = `/query?=${name}&${selected[0]}&${selected[1]}&${selected[2]}`;
   console.log(url);
   domRequest(url);
-})
+});
 
-const resToFrontEnd = (input, json) => {
+const domRequest = url => {
+  let xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      responseOutput = JSON.parse(xhr.responseText);
+      resToFrontEnd(responseOutput);
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+};
+
+const resToFrontEnd = json => {
   //Removes old data
-
   //Case if results are NULL
-
   // Case populating the dom
-
-
-}
+};
