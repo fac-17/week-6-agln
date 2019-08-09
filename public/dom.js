7c5a0025cd78e2442067e2a7d3f61319b7a2a67b
gminova <minova1995@gmail.com>
2019-08-09 11:03:46 +0100

    deleted domjs fixes #30

--- public/dom.js
+++
@@ -1,6 +0,0 @@
-// const submitForm = document.querySelector(".form__submit");
-//
-// submitForm.addEventlistener("click", e => {
-//   const memberName = document.querySelector(".form__input");
-//   const
-// });
